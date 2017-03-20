8447fadd21d2889209ca3ecf80d85b3c2c15027a
maxvanbel <maxvanbel@gmail.com>
2017-03-20 17:59:06 -0500

    work on local host

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 // Dependencies
 var express = require("express");
+var path = require("path");
 // Sets up the Express App
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, '/public')));
+
 app.get('/', function(req, res) {
-    res.send('index.html');
+    res.sendFile('index.html');
 });
 // Starts the server to begin listening
 app.listen(PORT, function() {
